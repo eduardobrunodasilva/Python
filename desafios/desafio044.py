2aa9b30f39d84155712ff921df64b92fb307d451
Eduardo Bruno <eduardobruno.dasilvabarbosa@gmail.com>
2025-05-16 16:51:11 -0300

    Novos desafios

--- desafios/desafio044.py
+++ desafios/desafio044.py
@@ -3,3 +3,10 @@ pelo teclado. O programa só vai parar quando o usuário digitar
 o valor 999, que é a condição de parada. No final, mostre
 quantos números foram digitados e qual foi a soma entre eles
 (desconsiderando o flag (999))'''
+n = cont = soma = 0
+n = int(input('Digite um número [999 para parar]'))
+while n != 999:
+    soma += n
+    cont += 1
+    n = int(input('Digite um número [999 para parar]'))
+print('Você digitou {} números e a soma entre eles foi {}'.format(cont, soma))
--- desafios/desafio045.py
+++ desafios/desafio045.py
@@ -3,3 +3,22 @@ teclado. No final da execução, mostre a média entre todos
 os valores e qual foi o maior e o menor valores lidos.
 O programa deve perguntar ao usuário se ele quer ou
 não continuar a digitar os valores'''
+
+resp = 'S'
+soma = quant = média = maior = menor = 0
+while resp in 'Ss':
+    n = int(input('Digite um número: '))
+    soma += n
+    quant += 1
+    if quant == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+média = soma / quant
+print('Você digitou {} números e média foi {}'.format(quant, média))
+print('O maior valor foi {} e o menor foi {}'.format(maior,menor))
+
