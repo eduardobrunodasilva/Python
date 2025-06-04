c5369bdfb28a569079c88d1620d7429b3c636169
Eduardo Bruno <eduardobruno.dasilvabarbosa@gmail.com>
2025-06-04 07:12:46 -0300

    exercícios resolvidos

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -3,3 +3,20 @@ em uma lista. Depois disso, crie duas listas extras que vão
 conter apenas os valores pares e os valores ímpares digitados,
 respectivamente. Ao final, mostre o conteúdo das três listas
 geradas.'''
+lista = []
+pares = list()
+ímpares = list()
+while True:
+    lista.append(int(input('Digite um número:')))
+    resp = str(input('Quer continuar? [S/N]: '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(lista):
+    if v % 2 == 0:
+        pares.append(v)
+    else:
+        ímpares.append(v)
+print('-=' * 30)
+print(f'A lista completa é {lista}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de ímpares é {ímpares}')
--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -2,3 +2,18 @@
 qualquer que use parênteses. Seu aplicativo deverá analisar
 se a expressão passada está com os parênteses abertos e
 fechados na ordem correta'''
+expr = str(input('Digite a expressão: '))
+pilha = []
+for símb in expr:
+    if símb == '(':
+        pilha.append('(')
+    elif símb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão não é válida!')
